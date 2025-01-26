9cb383cda855ba63d251b8380aa19d11f90fd150
fredericoaugusto <fred3112.augusto@gmail.com>
2025-01-26 17:29:34 -0300

    Criação da view game junto dos componentes e answer com suas respectivas lógicas.

---
+++ resources/views/game.blade.php
@@ -0,0 +1,19 @@
+<x-main-layout pageTitle="Countries & Capitals Quiz">
+    <x-question :country="$country" :currentQuestion="$currentQuestion" :totalQuestions="$totalQuestions"/>
+
+    <div class="container">
+
+        <div class="row">
+            @foreach($answers as $answer)
+                <x-answer :capital="$answer"/>
+            @endforeach
+        </div>
+
+    </div>
+
+    <!-- cancel game -->
+    <div class="text-center mt-5">
+        <a href="#" class="btn btn-outline-danger mt-3 px-5">CANCELAR JOGO</a>
+    </div>
+
+</x-main-layout>
