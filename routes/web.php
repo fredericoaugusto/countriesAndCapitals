6d6e7d7939034de6846de3915adab7c9a60c6934
fredericoaugusto <fred3112.augusto@gmail.com>
2025-01-26 17:28:54 -0300

    Criação da rota do game

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [MainController::class, 'startGame'])->name('start_game');
 Route::post('/', [MainController::class, 'prepareGame'])->name('prepare_game');
+
+Route::get('/game', [MainController::class, 'game'])->name('game');
